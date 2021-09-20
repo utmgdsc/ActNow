e4be5387a8a53f7891c199cea6fd340602ac276b
Syed Mohammed <syed.mohammed@mail.utoronto.ca>
2021-09-20 19:35:21 -0400

    Barebone login and signup page

---
+++ src/app/lib/signup_screen.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatefulWidget {
+  const SignupPage({Key? key}) : super(key: key);
+
+  @override
+  _SignupPageState createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Stack(
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(15.0, 180.0, 0.0, 0.0),
+                    child: const Text('Create your Account',
+                        style: TextStyle(
+                            fontSize: 30.0, fontWeight: FontWeight.bold)),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(16.0, 215, 0.0, 0.0),
+                    child: const Text('Sign up to start using ActNow',
+                        style: TextStyle(fontSize: 15.0)),
+                  ),
+                ],
+              ),
+              Container(
+                  padding:
+                      const EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+                  child: Column(
+                    children: <Widget>[
+                      const TextField(
+                        decoration: InputDecoration(
+                            labelText: 'EMAIL',
+                            labelStyle: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.green))),
+                      ),
+                      const SizedBox(height: 10.0),
+                      const TextField(
+                        decoration: InputDecoration(
+                            labelText: 'USERNAME ',
+                            labelStyle: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.green))),
+                      ),
+                      const SizedBox(height: 10.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const <Widget>[
+                          Flexible(
+                            child: Padding(
+                              padding: EdgeInsets.fromLTRB(0, 0, 5, 0),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    labelText: 'FIRST NAME ',
+                                    labelStyle: TextStyle(
+                                        fontFamily: 'Montserrat',
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.grey),
+                                    focusedBorder: UnderlineInputBorder(
+                                        borderSide:
+                                            BorderSide(color: Colors.green))),
+                              ),
+                            ),
+                          ),
+                          Flexible(
+                            child: Padding(
+                              padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    labelText: 'LAST NAME ',
+                                    labelStyle: TextStyle(
+                                        fontFamily: 'Montserrat',
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.grey),
+                                    focusedBorder: UnderlineInputBorder(
+                                        borderSide:
+                                            BorderSide(color: Colors.green))),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 10.0),
+                      const TextField(
+                        decoration: InputDecoration(
+                            labelText: 'PASSWORD ',
+                            labelStyle: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.green))),
+                        obscureText: true,
+                      ),
+                      const SizedBox(height: 50.0),
+                      SizedBox(
+                          height: 40.0,
+                          child: Material(
+                            borderRadius: BorderRadius.circular(20.0),
+                            shadowColor: Colors.greenAccent,
+                            color: Colors.green,
+                            elevation: 7.0,
+                            child: GestureDetector(
+                              onTap: () {},
+                              child: const Center(
+                                child: Text(
+                                  'SIGNUP',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: 'Montserrat'),
+                                ),
+                              ),
+                            ),
+                          )),
+                      const SizedBox(height: 20.0),
+                    ],
+                  )),
+              const SizedBox(height: 15.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text(
+                    'Already have an account?',
+                    style: TextStyle(fontFamily: 'Montserrat'),
+                  ),
+                  const SizedBox(width: 5.0),
+                  InkWell(
+                    onTap: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('Login in',
+                        style: TextStyle(
+                            color: Colors.green,
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold)),
+                  )
+                ],
+              )
+            ]));
+  }
+}
