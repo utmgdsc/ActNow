1fc749e4df89a850529e8a2274570bbb4a66cc46
Jason Chan <j9t11@hotmail.com>
2021-09-21 16:12:44 -0400

    Added the base firebase files (#23)
    * Added the base firebase files
    
    * Docs - Added documentation to our project (#22)
    
    * adding boilerplate
    
    * completed monorepo setup
    
    * added gitignore
    
    * removed .DS_Store
    
    * added docs page
    
    * fixed typo
    
    * updated home page images
    
    * add build files for github pages
    
    * Testing pr template (#6)
    
    * test change
    
    * fixed pr template location
    
    * Update README.md (#8)
    
    * Adding firestore connection test PR(#10)
    
    * Barebone login and signup page (#18)
    
    * renamed documentation to docs
    
    * more renaming
    
    Co-authored-by: Siraj Mohammed <syed.mohammed@mail.utoronto.ca>
    
    Co-authored-by: Shawn Hu <shawnhu97@gmail.com>
    Co-authored-by: Siraj Mohammed <syed.mohammed@mail.utoronto.ca>

--- src/app/lib/main.dart
+++ src/app/lib/main.dart
@@ -1,21 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'login_screen.dart';
 import 'signup_screen.dart';
 
-void main() => runApp(const MyApp());
+//void main() => runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  final Future<FirebaseApp> _firebaseApp = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: <String, WidgetBuilder>{
-        '/signup': (BuildContext context) => const SignupPage(),
-      },
-      home: const LoginPage(),
-    );
+        debugShowCheckedModeBanner: false,
+        routes: <String, WidgetBuilder>{
+          '/signup': (BuildContext context) => const SignupPage(),
+        },
+        home: FutureBuilder(
+          future: _firebaseApp,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return const Text('Opps! Something Went Wrong');
+            } else if (snapshot.hasData) {
+              return const LoginPage();
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ));
   }
 }
