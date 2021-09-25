bcf13a0cdcea86e529cf5ded849572b5bbdcd0c2
TheFatPanda97 <shawnhu97@gmail.com>
2021-09-25 13:21:54 -0400

    added babel
    Co-authored-by: lx1922 <lx1922@users.noreply.github.com>

---
+++ src/api/dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var _express = _interopRequireDefault(require("express"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const app = (0, _express.default)();
+const port = 3000;
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
