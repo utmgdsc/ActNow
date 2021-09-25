ff851d6610e557d4e8d6f780fa1c26553a1b3881
TheFatPanda97 <shawnhu97@gmail.com>
2021-09-25 19:58:04 -0400

    fixed docs order

--- src/documentation/src/pages/index.jsx
+++ src/documentation/src/pages/index.jsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Download Now
+            See The Docs
           </Link>
         </div>
       </div>
