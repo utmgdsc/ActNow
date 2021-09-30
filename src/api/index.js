60cea2f66c9bd04fda619d36e78160337e29bb78
Shrey Vakil <43979159+lx1922@users.noreply.github.com>
2021-09-29 21:33:43 -0400

    Feat/event server (#34)
    * inital sertup
    
    * added nodemon
    
    * added babel
    
    Co-authored-by: lx1922 <lx1922@users.noreply.github.com>
    
    * updated babel config
    
    * removed build
    
    * added eslint
    
    * Add files via upload
    
    * Created basic event-scraper template
    
    * added async await capabilites
    
    * Can scrape eventbrite website to get event details
    
    * Fixed some issues -> json not having correct value
    
    * Removed comments and useless files
    
    * fixed some linting problems
    
    * fixed some more issues
    
    Co-authored-by: TheFatPanda97 <shawnhu97@gmail.com>
    Co-authored-by: lx1922 <lx1922@users.noreply.github.com>
    Co-authored-by: Goutam <76483581+Goutam112@users.noreply.github.com>
    Co-authored-by: Goutam112 <goutams112345@gmail.com>
    Co-authored-by: Vakil <Shrey.Vakil@ceridian.com>

---
+++ src/api/index.js
@@ -0,0 +1,84 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import express from 'express';
+import puppeteer from 'puppeteer';
+
+const app = express();
+const port = 3000;
+
+const timeout = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+app.get('/', (req, res) => {
+  let eventsArray = [];
+  (async () => {
+    const webpageUrl = 'https://www.eventbrite.ca';
+
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.setViewport({
+      width: 1920,
+      height: 1080,
+    });
+
+    await page.goto(webpageUrl, { waitUntil: 'networkidle2' });
+
+    await timeout(3000);
+
+    await page.type('#locationPicker', 'Toronto');
+
+    await page.keyboard.press('Enter');
+
+    await timeout(3000);
+
+    eventsArray = await page.evaluate(() => {
+      const events = [];
+      for (let i = 1; i < 9; i += 1) {
+        console.log(i);
+        const eventTitle = document.querySelector(
+          '#panel0 > div > div.feed-events-bucket.feed-events--primary_bucket > div.feed-events-bucket__content > div:nth-child(' +
+            i.toString() +
+            ') > div > div > article > div.eds-event-card-content__content-container.eds-event-card-content__content-container--consumer > div.eds-event-card-content__content > div > div.eds-event-card-content__primary-content > a > h3 > div > div.eds-event-card__formatted-name--is-clamped.eds-event-card__formatted-name--is-clamped-three.eds-text-weight--heavy',
+        );
+        const eventDate = document.querySelector(
+          '#panel0 > div > div.feed-events-bucket.feed-events--primary_bucket > div.feed-events-bucket__content > div:nth-child(' +
+            i.toString() +
+            ') > div > div > article > div.eds-event-card-content__content-container.eds-event-card-content__content-container--consumer > div.eds-event-card-content__content > div > div.eds-event-card-content__primary-content > div',
+        );
+        const eventLoc = document.querySelector(
+          '#panel0 > div > div.feed-events-bucket.feed-events--primary_bucket > div.feed-events-bucket__content > div:nth-child(' +
+            i.toString() +
+            ') > div > div > article > div.eds-event-card-content__content-container.eds-event-card-content__content-container--consumer > div.eds-event-card-content__content > div > div.eds-event-card-content__sub-content > div:nth-child(1) > div',
+        );
+        const organizedBy = document.querySelector(
+          '#panel0 > div > div.feed-events-bucket.feed-events--primary_bucket > div.feed-events-bucket__content > div:nth-child(' +
+            i.toString() +
+            ') > div > div > article > div.eds-event-card-content__content-container.eds-event-card-content__content-container--consumer > div.eds-event-card-content__content > div > div.eds-event-card-content__sub-content > div:nth-child(3) > div > div.eds-event-card__sub-content--organizer.eds-text-color--ui-800.eds-text-weight--heavy.card-text--truncated__two',
+        );
+        const cost = document.querySelector(
+          '#panel0 > div > div.feed-events-bucket.feed-events--primary_bucket > div.feed-events-bucket__content > div:nth-child(' +
+            i.toString() +
+            ') > div > div > article > div.eds-event-card-content__content-container.eds-event-card-content__content-container--consumer > div.eds-event-card-content__content > div > div.eds-event-card-content__sub-content > div:nth-child(2)',
+        );
+        events.push({
+          title: eventTitle ? eventTitle.innerHTML : '',
+          date: eventDate ? eventDate.innerHTML : '',
+          location: eventLoc ? eventLoc.innerHTML : '',
+          ticket: cost ? cost.innerHTML : '',
+          organization: organizedBy ? organizedBy.innerHTML : '',
+          // img: eventImg,
+          // description: eventDesc
+        });
+      }
+
+      return events;
+    });
+
+    console.log(eventsArray);
+    res.send(eventsArray);
+    // await browser.close();
+  })();
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
