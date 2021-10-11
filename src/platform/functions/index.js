4c748a15bf91844e75be96de5f6a33ca8082e9b7
Shawn Hu <shawnhu97@gmail.com>
2021-10-10 20:35:59 -0400

    increased memory usage

--- src/platform/functions/index.js
+++ src/platform/functions/index.js
@@ -9,7 +9,7 @@ const timeout = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 exports.scrapeEventbrite = functions
   .runWith({
     timeoutSeconds: 30,
-    memory: '256MB',
+    memory: '1GB',
   })
   .https.onRequest(async (_, res) => {
     functions.logger.info('Starting to scrape...');
