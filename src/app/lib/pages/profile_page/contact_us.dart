46bbb17c37b1b2a203bc39a62a30b13848d74b87
Syed Mohammed <syed.mohammed@mail.utoronto.ca>
2021-10-17 21:39:09 -0400

    remove unused imports

--- src/app/lib/pages/profile_page/contact_us.dart
+++ src/app/lib/pages/profile_page/contact_us.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 class ContactUs extends StatefulWidget {
   const ContactUs({Key? key}) : super(key: key);
--- src/app/lib/pages/profile_page/personal_info.dart
+++ src/app/lib/pages/profile_page/personal_info.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 class PersonalInfo extends StatefulWidget {
   final Map<String, dynamic>? userInfo;
