887cb83cbb9b4d9a30703fdee91b929e41ce1cc4
Jason Chan <j9t11@hotmail.com>
2021-10-24 21:40:17 -0400

    Revert "Revert "Application - added event widget (#88)" (#89)"
    This reverts commit 971c47c260233e950d75c41e953b675bbf7d6f8d.

--- src/app/lib/pages/explore_page.dart
+++ src/app/lib/pages/explore_page.dart
@@ -1,3 +1,4 @@
+import 'package:actnow/pages/event_widget.dart';
 import 'package:flutter/material.dart';
 
 class ExplorePage extends StatefulWidget {
@@ -12,7 +13,38 @@ class ExplorePageState extends State<ExplorePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-            padding: const EdgeInsets.fromLTRB(150.0, 180.0, 0.0, 0.0),
-            child: const Text("Explore Page")));
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+            child: Column(
+              children: [
+                Expanded(
+                  child: ListView(
+                    children: [
+                      EventWidget(
+                        title: "test title",
+                        creator: "test creator 232",
+                        date_time: DateTime.now(),
+                        num_attendees: 23,
+                      ),
+                      EventWidget(
+                        title: "test title",
+                        creator: "test creator",
+                        date_time: DateTime.now(),
+                        num_attendees: 223,
+                        img_location: 'https://i.imgur.com/jaPAgQH.jpeg',
+                      ),
+                      EventWidget(
+                        title: "test title",
+                        creator: "test creator 4251255",
+                        date_time: DateTime.now(),
+                        num_attendees: 23,
+                        img_location:
+                            'https://img.bleacherreport.net/img/images/photos/003/813/911/hi-res-76dc39978a7762c48eab1955cb58d65f_crop_north.jpg?1560490545&w=3072&h=2048',
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            )));
   }
 }
