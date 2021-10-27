dc8daa63fb15a8c020beb759e0ab03aea122c76d
Jason Chan <j9t11@hotmail.com>
2021-10-27 06:23:25 -0400

    Issue with the snapshots..

--- src/app/lib/pages/explore_page.dart
+++ src/app/lib/pages/explore_page.dart
@@ -22,30 +22,24 @@ class ExplorePageState extends State<ExplorePage> {
             child: Column(
               children: [
                 Expanded(
-                  child: ListView(
-                    children: [
-                      EventWidget(
-                        title: "test title",
-                        creator: "test creator 232",
-                        date_time: DateTime.now(),
-                        num_attendees: 23,
-                      ),
-                      EventWidget(
-                        title: "test title",
-                        creator: "test creator",
-                        date_time: DateTime.now(),
-                        num_attendees: 223,
-                        img_location: 'https://i.imgur.com/jaPAgQH.jpeg',
-                      ),
-                      EventWidget(
-                        title: "test title",
-                        creator: "test creator 4251255",
-                        date_time: DateTime.now(),
-                        num_attendees: 23,
-                        img_location:
-                            'https://img.bleacherreport.net/img/images/photos/003/813/911/hi-res-76dc39978a7762c48eab1955cb58d65f_crop_north.jpg?1560490545&w=3072&h=2048',
-                      )
-                    ],
+                  child: FutureBuilder(
+                    initialData: [],
+                    future: getEventData(),
+                    builder: (context, snapshot) {
+                      return ListView.builder(
+                          //itemCount: snapshot.data!.length,
+                          itemCount: 2,
+                          itemBuilder: (context, index) {
+                            return EventWidget(
+                              //title: snapshot.data![index].title,
+                              title: "testssss",
+                              creator: "test creator",
+                              date_time: DateTime.now(),
+                              num_attendees: 223,
+                              img_location: 'https://i.imgur.com/jaPAgQH.jpeg',
+                            );
+                          });
+                    },
                   ),
                 ),
               ],
@@ -78,7 +72,7 @@ class EventDetails {
   }
 }
 
-Future<List<EventDetails>> getTasks() async {
+Future<List<EventDetails>> getEventData() async {
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
   List<Map<String, dynamic>> ref = firestore
       .collection('events')
