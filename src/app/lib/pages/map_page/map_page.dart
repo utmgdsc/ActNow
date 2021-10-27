9638c5b69a47642f20e1ebe7fc6d120c85ba7469
Shawn Hu <shawnhu97@gmail.com>
2021-10-27 17:53:38 -0400

    small fixes

--- src/app/lib/pages/map_page/map_page.dart
+++ src/app/lib/pages/map_page/map_page.dart
@@ -1,9 +1,6 @@
 import 'dart:async';
-<<<<<<< HEAD
 import 'dart:io';
-=======
 import 'package:actnow/pages/event_details.dart';
->>>>>>> master
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
