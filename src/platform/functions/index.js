488e22c5cea9c6ed12a7477a8a4832ed267e4e4f
lx1922 <lx1922@users.noreply.github.com>
2021-10-28 00:50:13 -0400

    Added scraping for 60 events & moved overhauled server to functions

--- src/platform/functions/index.js
+++ src/platform/functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
-const puppeteer = require('puppeteer');
+// const puppeteer = require('puppeteer');
 const admin = require('firebase-admin');
+const { Cluster } = require('puppeteer-cluster');
 
 admin.initializeApp();
 
@@ -14,90 +15,88 @@ exports.scrapeEventbrite = functions
   .https.onRequest(async (_, res) => {
     functions.logger.info('Starting to scrape...');
     let eventsArray = [];
-    const webpageUrl = 'https://www.eventbrite.ca';
 
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
-    await page.setViewport({
-      width: 1920,
-      height: 1080,
-    });
+    (async () => {
+      const cluster = await Cluster.launch({
+        concurrency: Cluster.CONCURRENCY_CONTEXT,
+        maxConcurrency: 3,
+      });
 
-    await page.goto(webpageUrl, { waitUntil: 'networkidle2' });
-    await page.type('#locationPicker', 'Toronto');
-    await page.keyboard.press('Enter');
-    await timeout(2000);
+      await cluster.task(async ({ page, data: webpageUrl }) => {
+        await page.setViewport({
+          width: 1920,
+          height: 1080,
+        });
 
-    eventsArray = await page.evaluate(() => {
-      const events = [];
-      for (let i = 1; i < 9; i += 1) {
-        const eventTitle = document.querySelector(
-          `#panel0 > div > div.feed-events-bucket.feed-events--primary_bucket > div.feed-events-bucket__content > div:nth-child(${i.toString()}) > div > div > article > div.eds-event-card-content__content-container.eds-event-card-content__content-container--consumer > div.eds-event-card-content__content > div > div.eds-event-card-content__primary-content > a > h3 > div > div.eds-event-card__formatted-name--is-clamped.eds-event-card__formatted-name--is-clamped-three.eds-text-weight--heavy`,
-        );
-        const eventDate = document.querySelector(
-          `#panel0 > div > div.feed-events-bucket.feed-events--primary_bucket > div.feed-events-bucket__content > div:nth-child(${i.toString()}) > div > div > article > div.eds-event-card-content__content-container.eds-event-card-content__content-container--consumer > div.eds-event-card-content__content > div > div.eds-event-card-content__primary-content > div`,
-        );
-        const eventLoc = document.querySelector(
-          `#panel0 > div > div.feed-events-bucket.feed-events--primary_bucket > div.feed-events-bucket__content > div:nth-child(${i.toString()}) > div > div > article > div.eds-event-card-content__content-container.eds-event-card-content__content-container--consumer > div.eds-event-card-content__content > div > div.eds-event-card-content__sub-content > div:nth-child(1) > div.card-text--truncated__one`,
-        );
-        const organizedBy = document.querySelector(
-          `#panel0 > div > div.feed-events-bucket.feed-events--primary_bucket > div.feed-events-bucket__content > div:nth-child(${i.toString()}) > div > div > article > div.eds-event-card-content__content-container.eds-event-card-content__content-container--consumer > div.eds-event-card-content__content > div > div.eds-event-card-content__sub-content > div > div > div.eds-event-card__sub-content--organizer.eds-text-color--ui-800.eds-text-weight--heavy.card-text--truncated__two`,
-        );
-        const cost = document.querySelector(
-          `#panel0 > div > div.feed-events-bucket.feed-events--primary_bucket > div.feed-events-bucket__content > div:nth-child(${i.toString()}) > div > div > article > div.eds-event-card-content__content-container.eds-event-card-content__content-container--consumer > div.eds-event-card-content__content > div > div.eds-event-card-content__sub-content > div:nth-child(2)`,
-        );
-        const imgUrl = document.querySelector(
-          `#panel0 > div > div.feed-events-bucket.feed-events--primary_bucket > div.feed-events-bucket__content > div:nth-child(${i.toString()}) > div > div > article > aside.eds-event-card-content__image-container > a.eds-event-card-content__action-link img`,
-        );
-        const eventUrl = document.querySelector(
-          `#panel0 > div > div.feed-events-bucket.feed-events--primary_bucket > div.feed-events-bucket__content > div:nth-child(${i.toString()}) > div > div > article > aside.eds-event-card-content__image-container > a.eds-event-card-content__action-link`,
-        );
+        await page.goto(webpageUrl, { waitUntil: 'networkidle2' });
+        await timeout(2000);
 
-        const newEvent = {
-          title: eventTitle ? eventTitle.innerText : '',
-          date: eventDate ? eventDate.innerText : '',
-          location: eventLoc ? eventLoc.innerText : '',
-          ticket: cost ? cost.innerText : '',
-          organization: organizedBy ? organizedBy.innerText : '',
-          img: imgUrl ? imgUrl.getAttribute('src') : '',
-          url: eventUrl ? eventUrl.getAttribute('href') : '',
-        };
+        eventsArray = await page.evaluate(async () => {
+          const events = [];
+          // for (let x = 1; x <= 3; x += 1) {
+          for (let i = 1; i <= 60; i += 1) {
+            // console.log(i);
+            const eventTitle = document.querySelector(`#root > div > div.eds-structure__body > div > div > div > div.eds-fixed-bottom-bar-layout__content > div > main > div > div > section.search-base-screen__search-panel > div.search-results-panel-content > div > ul > li:nth-child(${i.toString()}) > div > div > div.search-event-card-rectangle-image > div > div > div > article > div.eds-event-card-content__content-container.eds-l-pad-right-4 > div > div > div.eds-event-card-content__primary-content > a > h3 > div > div.eds-event-card__formatted-name--is-clamped.eds-event-card__formatted-name--is-clamped-three.eds-text-weight--heavy`);
+            const eventDate = document.querySelector(`#root > div > div.eds-structure__body > div > div > div > div.eds-fixed-bottom-bar-layout__content > div > main > div > div > section.search-base-screen__search-panel > div.search-results-panel-content > div > ul > li:nth-child(${i.toString()}) > div > div > div.search-event-card-rectangle-image > div > div > div > article > div.eds-event-card-content__content-container.eds-l-pad-right-4 > div > div > div.eds-event-card-content__primary-content > div`);
+            const eventLoc = document.querySelector(`#root > div > div.eds-structure__body > div > div > div > div.eds-fixed-bottom-bar-layout__content > div > main > div > div > section.search-base-screen__search-panel > div.search-results-panel-content > div > ul > li:nth-child(${i.toString()}) > div > div > div.search-event-card-rectangle-image > div > div > div > article > div.eds-event-card-content__content-container.eds-l-pad-right-4 > div > div > div.eds-event-card-content__sub-content > div:nth-child(1) > div`);
+            const cost = document.querySelector(`#root > div > div.eds-structure__body > div > div > div > div.eds-fixed-bottom-bar-layout__content > div > main > div > div > section.search-base-screen__search-panel > div.search-results-panel-content > div > ul > li:nth-child(${i.toString()}) > div > div > div.search-event-card-rectangle-image > div > div > div > article > div.eds-event-card-content__content-container.eds-l-pad-right-4 > div > div > div.eds-event-card-content__sub-content > div:nth-child(2)`);
+            const organizedBy = document.querySelector(`#root > div > div.eds-structure__body > div > div > div > div.eds-fixed-bottom-bar-layout__content > div > main > div > div > section.search-base-screen__search-panel > div.search-results-panel-content > div > ul > li:nth-child(${i.toString()}) > div > div > div.search-event-card-rectangle-image > div > div > div > article > div.eds-event-card-content__content-container.eds-l-pad-right-4 > div > div > div.eds-event-card-content__sub-content > div:nth-child(3) > div > div.eds-event-card__sub-content--organizer.eds-text-color--ui-800.eds-text-weight--heavy.card-text--truncated__two`);
+            const imgUrl = document.querySelector(`#root > div > div.eds-structure__body > div > div > div > div.eds-fixed-bottom-bar-layout__content > div > main > div > div > section.search-base-screen__search-panel > div.search-results-panel-content > div > ul > li:nth-child(${i.toString()}) > div > div > div.search-event-card-rectangle-image > div > div > div > article > div.image-action-container > aside > a > div > div > img`);
+            const eventUrl = document.querySelector(`#root > div > div.eds-structure__body > div > div > div > div.eds-fixed-bottom-bar-layout__content > div > main > div > div > section.search-base-screen__search-panel > div.search-results-panel-content > div > ul > li:nth-child(${i.toString()}) > div > div > div.search-event-card-rectangle-image > div > div > div > article > div.eds-event-card-content__content-container.eds-l-pad-right-4 > div > div > div.eds-event-card-content__primary-content > a`);
 
-        if (
-          !(
-            newEvent.ticket.substring(0, 4) === 'Free' ||
-            newEvent.ticket.substring(0, 9) === 'Starts at'
-          )
-        ) {
-          newEvent.ticket = '';
+            const newEvent = {
+              title: eventTitle ? eventTitle.innerText : '',
+              date: eventDate ? eventDate.innerText : '',
+              location: eventLoc ? eventLoc.innerText : '',
+              ticket: cost ? cost.innerText : '',
+              organization: organizedBy ? organizedBy.innerText : '',
+              img: imgUrl ? imgUrl.getAttribute('src') : '',
+              url: eventUrl ? eventUrl.getAttribute('href') : '',
+            };
+
+            if (
+              !(
+                newEvent.ticket.substring(0, 4) === 'Free' ||
+                newEvent.ticket.substring(0, 9) === 'Starts at'
+              )
+            ) {
+              newEvent.ticket = '';
+            }
+
+            if (!(newEvent.title === '')) {
+              events.push(newEvent);
+            }
+          }
+          return events;
+        });
+
+        if (eventsArray.length === 0) {
+          functions.logger.error('No events scrapped');
+        } else {
+          functions.logger.info('Number of scrapper events:', eventsArray.length);
         }
 
-        if (!(newEvent.title === '')) {
-          events.push(newEvent);
-        }
-      }
-      return events;
-    });
+        eventsArray.forEach((event) => {
+          (async () => {
+            await admin
+              .firestore()
+              .collection('events')
+              .doc('eventbrite')
+              .collection('toronto')
+              .add(event);
+          })();
+        });
+      });
 
-    if (eventsArray.length === 0) {
-      functions.logger.error('No events scrapped');
-    } else {
-      functions.logger.info('Number of scrapper events:', eventsArray.length);
-    }
+      cluster.queue('https://www.eventbrite.ca/d/Toronto/all-events/?page=1');
+      cluster.queue('https://www.eventbrite.ca/d/Toronto/all-events/?page=2');
+      cluster.queue('https://www.eventbrite.ca/d/Toronto/all-events/?page=3');
+      // many more pages
 
-    eventsArray.forEach((event) => {
-      (async () => {
-        await admin
-          .firestore()
-          .collection('events')
-          .doc('eventbrite')
-          .collection('toronto')
-          .add(event);
-      })();
-    });
+      await cluster.idle();
+      await cluster.close();
+    })();
 
     functions.logger.info('Uploaded to firestore');
-
-    await browser.close();
     res.send(eventsArray);
   });
