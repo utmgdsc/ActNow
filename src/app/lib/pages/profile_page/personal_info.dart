5414c16e2ae3892475baed94f79e1b55a05d9e98
Syed Mohammed <syed.mohammed@mail.utoronto.ca>
2021-11-13 22:18:51 -0500

    edit profile picture

--- src/app/lib/pages/profile_page/personal_info.dart
+++ src/app/lib/pages/profile_page/personal_info.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:uuid/uuid.dart';
 
 class PersonalInfo extends StatefulWidget {
   final Map<String, dynamic>? userInfo;
@@ -11,6 +16,9 @@ class PersonalInfo extends StatefulWidget {
 }
 
 class PersonalInfoState extends State<PersonalInfo> {
+  final ImagePicker _picker = ImagePicker();
+  XFile? _image;
+
   final GlobalKey<FormState> _formKey =
       GlobalKey<FormState>(); // Required for form validator
 
@@ -32,7 +40,6 @@ class PersonalInfoState extends State<PersonalInfo> {
   updateUserInfo(BuildContext context) async {
     if (_formKey.currentState!.validate()) {
       DocumentReference ref = users.doc(widget.userInfo!["userid"]);
-
       if (_firstName.text.isNotEmpty &&
           _firstName.text != widget.userInfo!["firstname"]) {
         ref.update({'firstname': _firstName.text});
@@ -42,8 +49,74 @@ class PersonalInfoState extends State<PersonalInfo> {
         ref.update({'lastname': _lastName.text});
       }
 
+      if (_image != null) {
+        var data = await ref.get();
+        var fileRef = firebase_storage.FirebaseStorage.instance
+            .ref(const Uuid().v4().toString());
+        try {
+          await fileRef.putFile(File(_image!.path));
+        } catch (e) {
+          // e.g, e.code == 'canceled'
+        }
+        var imageUrl = await fileRef.getDownloadURL();
+
+        var dataMap = data.data() as Map<String, dynamic>;
+        if (dataMap.containsKey("profile_picture")) {
+          await FirebaseStorage.instance
+              .refFromURL(dataMap["profile_picture"])
+              .delete();
+        }
+        ref.update({'profile_picture': imageUrl});
+      }
+
       Navigator.of(context).pop();
     }
+  }
+
+  void _imgFromCamera() async {
+    XFile? image =
+        await _picker.pickImage(source: ImageSource.camera, imageQuality: 50);
+
+    setState(() {
+      _image = image;
+    });
+  }
+
+  void _imgFromGallery() async {
+    XFile? image =
+        await _picker.pickImage(source: ImageSource.gallery, imageQuality: 50);
+
+    setState(() {
+      _image = image;
+    });
+  }
+
+  void _showPicker(context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext bc) {
+          return SafeArea(
+            child: Wrap(
+              children: <Widget>[
+                ListTile(
+                    leading: const Icon(Icons.photo_library),
+                    title: const Text('Photo Library'),
+                    onTap: () {
+                      _imgFromGallery();
+                      Navigator.of(context).pop();
+                    }),
+                ListTile(
+                  leading: const Icon(Icons.photo_camera),
+                  title: const Text('Camera'),
+                  onTap: () {
+                    _imgFromCamera();
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            ),
+          );
+        });
   }
 
   @override
@@ -64,9 +137,12 @@ class PersonalInfoState extends State<PersonalInfo> {
                         width: 130,
                         height: 130,
                         child: CircleAvatar(
-                          backgroundImage: NetworkImage(widget
-                                  .userInfo!["profile_picture"] ??
-                              "https://profilepicturemaker.com/wp-content/themes/ppm2021/images/transparent.gif"),
+                          backgroundImage: _image == null
+                              ? NetworkImage(widget
+                                          .userInfo!["profile_picture"] ??
+                                      "https://profilepicturemaker.com/wp-content/themes/ppm2021/images/transparent.gif")
+                                  as ImageProvider
+                              : FileImage(File(_image!.path)),
                         ),
                         decoration: BoxDecoration(
                             shape: BoxShape.circle,
@@ -95,10 +171,14 @@ class PersonalInfoState extends State<PersonalInfo> {
                             ),
                             color: Colors.blue,
                           ),
-                          child: const Icon(
-                            Icons.edit,
-                            color: Colors.white,
-                          ),
+                          child: GestureDetector(
+                              onTap: () {
+                                _showPicker(context);
+                              },
+                              child: const Icon(
+                                Icons.edit,
+                                color: Colors.white,
+                              )),
                         )),
                   ],
                 ),
